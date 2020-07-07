30a679ed28cc02ef7e9945e34f528d4230558872
Marc Villacorta <mvillacorta@newrelic.com>
2020-07-07 12:39:05 +0200

    kubebuilder create webhook --kind EKSConfig --programmatic-validation

--- bootstrap/eks/main.go
+++ bootstrap/eks/main.go
@@ -74,6 +74,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "EKSConfig")
 		os.Exit(1)
 	}
+	if err = (&bootstrapv1alpha3.EKSConfig{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "EKSConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
