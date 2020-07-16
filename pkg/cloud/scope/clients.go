14821978b84e45d7900b463ec85a13598c552930
nyson <nyson@newrelic.com>
2020-07-16 16:16:37 -0700

    chore: adds autoscaling iface

--- pkg/cloud/scope/clients.go
+++ pkg/cloud/scope/clients.go
@@ -17,6 +17,7 @@ limitations under the License.
 package scope
 
 import (
+	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/aws/aws-sdk-go/service/elb/elbiface"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi/resourcegroupstaggingapiiface"
@@ -25,6 +26,7 @@ import (
 
 // AWSClients contains all the aws clients used by the scopes.
 type AWSClients struct {
+	ASG             autoscalingiface.AutoScalingAPI
 	EC2             ec2iface.EC2API
 	ELB             elbiface.ELBAPI
 	SecretsManager  secretsmanageriface.SecretsManagerAPI
